7581274e72bb3c3518e0625ff1ae2ee534328b5b
Adrien Ragot <aragot@r-yogi.com>
2025-06-16 12:39:49 +0200

    Initialization

---
+++ plugin-a/src/main/java/com/requirementyogi/poc/plugin/a/rest/InfoResource.java
@@ -0,0 +1,30 @@
+package com.requirementyogi.poc.plugin.a.rest;
+
+import com.atlassian.confluence.core.ContextPathHolder;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+/**
+ * A resource to confirm that the plugin is up and running,
+ * and to confirm how serialization features behave.
+ * <p>
+ * Deployed at /rest/reqs/1/info
+ * */
+@Path("/info")
+public class InfoResource {
+
+    //@Autowired
+    @Inject
+    public InfoResource(
+    ) {
+    }
+
+    @GET
+    //@Produces(BaseUtils.JSON_UTF8)
+    public String info() {
+        return "Ok";
+    }
+}
